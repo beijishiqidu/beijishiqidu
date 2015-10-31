ae54c5bd3908334114ef0c788d73e66110479120
beijishiqidu <mb19890630@126.com>
2015-10-31 16:11:54 +0800

    提交爱情木瓜的方法。

--- personal.blog/src/test/java/personal/blog/test/git/AiQingMuGua.java
+++ personal.blog/src/test/java/personal/blog/test/git/AiQingMuGua.java
@@ -1,5 +1,12 @@
 package personal.blog.test.git;
 
+import org.apache.log4j.Logger;
+
 public class AiQingMuGua {
 
+	private static final Logger LOGGER = Logger.getLogger(AiQingMuGua.class);
+	
+	public static void main(String[] args) {
+		LOGGER.debug("开始执行main方法....");
+	}
 }
