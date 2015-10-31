33954ef3e4757037a5837e6e367e2db83705ce75
beijishiqidu <mb19890630@126.com>
2015-10-31 18:49:50 +0800

    删除了main方法，新增了新的方法。

--- personal.blog/src/test/java/personal/blog/test/git/BeiJiShiQiDu.java
+++ personal.blog/src/test/java/personal/blog/test/git/BeiJiShiQiDu.java
@@ -5,8 +5,8 @@ import org.apache.log4j.Logger;
 public class BeiJiShiQiDu {
 
     private static final Logger LOGGER = Logger.getLogger(BeiJiShiQiDu.class);
-
-    public static void main(String[] args) {
-        LOGGER.debug("执行main方法....");
+    
+    public static void addAiqingmugua(){
+    	LOGGER.debug("删除了main方法，增加了爱情木瓜的方法...");
     }
 }
