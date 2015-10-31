384476a08b220d68ddf56cf75b6457fdf88402a2
beijishiqidu <mb19890630@126.com>
2015-10-31 19:48:32 +0800

    测试回滚。

--- personal.blog/src/test/java/personal/blog/test/git/BeiJiShiQiDu.java
+++ personal.blog/src/test/java/personal/blog/test/git/BeiJiShiQiDu.java
@@ -6,16 +6,22 @@ public class BeiJiShiQiDu {
 
     private static final Logger LOGGER = Logger.getLogger(BeiJiShiQiDu.class);
 
-	public static void addAiqingmugua() {
-		LOGGER.debug("删除了main方法，增加了爱情木瓜的方法...");//我就是测试立即提交合并的改动的
-	}
+    public static void addAiqingmugua() {
+        LOGGER.debug("删除了main方法，增加了爱情木瓜的方法...");// 我就是测试立即提交合并的改动的
+    }
 
     public static void main(String[] args) {
         LOGGER.debug("执行main方法....");
 
         LOGGER.debug("修改了main里面的一些实现...");// TODO删除此方法....
     }
-    
-    
-    //测试push的功能的。
+
+
+    // 测试push的功能的。
+
+    // 还是测试push多个的功能。
+
+    public static void testRollback() {
+        LOGGER.debug("测试回滚");
+    }
 }
